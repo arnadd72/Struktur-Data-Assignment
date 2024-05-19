c9fe089e40efafa2ef7e2f9e139ffd48ca28a91b
arnadd72 <149177348+arnadd72@users.noreply.github.com>
2024-05-19 21:19:16 +0700

    Add files via upload

---
+++ MODUL 7 QUEUE/unguided modul 7/unguided 2.cpp
@@ -0,0 +1,116 @@
+//2311102180
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node
+{
+    string namaMahasiswa;
+    string nim;
+    Node *next;
+};
+
+Node *front = nullptr;
+Node *back = nullptr;
+
+bool isFull()
+{
+    return false;
+}
+
+bool isEmpty()
+{
+    return front == nullptr;
+}
+
+void enqueueAntrian(string nama, string nim)
+{
+    Node *newNode = new Node;
+    newNode->namaMahasiswa = nama;
+    newNode->nim = nim;
+    newNode->next = nullptr;
+
+    if (isEmpty())
+    {
+        front = newNode;
+        back = newNode;
+    }
+    else
+    {
+        back->next = newNode;
+        back = newNode;
+    }
+}
+
+void dequeueAntrian()
+{
+    if (isEmpty())
+    {
+        cout << "Queue Kosong" << endl;
+        return;
+    }
+
+    Node *temp = front;
+    front = front->next;
+    delete temp;
+
+    if (front == nullptr)
+    {
+        back = nullptr;
+    }
+}
+
+int countQueue()
+{
+    int count = 0;
+    Node *current = front;
+    while (current != nullptr)
+    {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+void clearQueue()
+{
+    while (front != nullptr)
+    {
+        Node *temp = front;
+        front = front->next;
+        delete temp;
+    }
+    back = nullptr;
+}
+
+void viewQueue()
+{
+    cout << "Data Queue Mahasiswa:" << endl;
+    Node *current = front;
+    while (current != nullptr)
+    {
+        cout << "Nama: " << current->namaMahasiswa << ", NIM: " << current->nim << endl;
+        current = current->next;
+    }
+}
+
+int main()
+{
+    enqueueAntrian("Abdul", "123456789");
+    enqueueAntrian("hasyim", "987654321");
+    viewQueue();
+    dequeueAntrian();
+    viewQueue();
+
+    cout << "Jumlah elemen dalam antrean: " << countQueue() << endl;
+    enqueueAntrian("Citra", "1122334455");
+    enqueueAntrian("Dewi", "5544332211");
+    viewQueue();
+    cout << "Jumlah elemen dalam antrean: " << countQueue() << endl;
+    clearQueue();
+    cout << "Antrean setelah clearQueue:" << endl;
+    viewQueue();
+
+    return 0;
+}
