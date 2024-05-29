1912ccf94aa371def44565ed8f0a8e4bd8eddfca
arnadd72 <149177348+arnadd72@users.noreply.github.com>
2024-05-29 17:12:17 +0700

    Add files via upload

---
+++ MODUL 9 GRAPH AND TREE/unguided modul 9/Unguided 1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    int jumlahSimpul2311102180;
+    cout << "Silakan masukkan jumlah simpul: ";
+    cin >> jumlahSimpul2311102180;
+
+    string simpul[jumlahSimpul2311102180];
+    int busur[jumlahSimpul2311102180][jumlahSimpul2311102180];
+
+    // Input nama simpul
+    for (int i = 0; i < jumlahSimpul2311102180; i++) {
+        cout << "Simpul " << i + 1 << ": ";
+        cin >> simpul[i];
+    }
+
+    // Input bobot antar simpul
+    for (int i = 0; i < jumlahSimpul2311102180; i++) {
+        for (int j = 0; j < jumlahSimpul2311102180; j++) {
+            cout << "Silakan masukkan bobot antara simpul " << simpul[i] << " dan " << simpul[j] << ": ";
+            cin >> busur[i][j];
+        }
+    }
+
+    // Menampilkan graf
+    cout << "\nGraf yang dihasilkan:\n";
+    cout << setw(15) << " ";
+    for (int i = 0; i < jumlahSimpul2311102180; i++) {
+        cout << setw(15) << simpul[i];
+    }
+    cout << endl;
+
+    for (int i = 0; i < jumlahSimpul2311102180; i++) {
+        cout << setw(15) << simpul[i];
+        for (int j = 0; j < jumlahSimpul2311102180; j++) {
+            cout << setw(15) << busur[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
