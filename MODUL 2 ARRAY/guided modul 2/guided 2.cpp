2ee428550309fe359cf6fb156eed189823e6dc34
arnadd72 <149177348+arnadd72@users.noreply.github.com>
2024-06-01 20:19:55 +0700

    Add files via upload

---
+++ MODUL 2 ARRAY/guided modul 2/guided 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int maks, a, i, lokasi = 0;
+    cout << "Masukkan panjang array: ";
+    cin >> a;
+    int array[a];
+    cout << "Masukkan " << a << " angka\n";
+    for (i = 0; i < a; i++)
+    {
+        cout << "Array ke-" << i << ": ";
+        cin >> array[i];
+    }
+
+    maks = array[0];
+    for (i = 1; i < a; i++)  
+    {
+        if (array[i] > maks)
+        {
+            maks = array[i];
+            lokasi = i;
+        }
+    }
+
+    cout << "Nilai maksimum adalah " << maks << " berada di array ke-" << lokasi << endl;
+    return 0;
+}
